6156ae37fc7553ec803f5492ffefaf43bb28ef1b
Salaheldin Akl <salaheldin.akl@student.dit.ie>
2019-04-09 19:34:41 +0100

    Update API domains

--- workflowstreamer-ui/src/constants/api.js
+++ workflowstreamer-ui/src/constants/api.js
@@ -1,2 +1,2 @@
-export const WORKFLOWSTREAMER_BASE = 'http://35.234.147.16';
-export const ANALYTICS_BASE = 'http://35.228.231.77';
+export const WORKFLOWSTREAMER_BASE = 'http://api.workflowstreamer.com';
+export const ANALYTICS_BASE = 'http://analytics.workflowstreamer.com';
